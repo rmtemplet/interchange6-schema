50338937e9bafc827142db65d496ffa01a3b14b7
Sam Batschelet <sbatschelet@mac.com>
2014-03-10 17:22:27 -0400

    Add shipping schema draft

---
+++ lib/Interchange6/Schema/Result/ShipmentDestination.pm
@@ -0,0 +1,132 @@
+use utf8;
+package Interchange6::Schema::Result::ShipmentDestination;
+
+=head1 NAME
+
+Interchange6::Schema::Result::ShipmentDestination;
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<shipment_destinations>
+
+=cut
+
+__PACKAGE__->table("shipment_destinations");
+
+=head1 ACCESSORS
+
+=head2 shipment_destinations
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 country_iso_code
+
+  data_type: 'varchar'
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 state_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 shipment_methods_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 active
+
+  data_type: 'boolean'
+  default_value: true
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "shipment_destinations_id",
+  {
+    data_type => "integer",
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "country_iso_code",
+  { data_type => "varchar", is_foreign_key => 1, is_nullable => 1 },
+  "state_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "shipment_methods_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "active",
+  { data_type => "boolean", default_value => \"true", is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</shipment_destinations_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("shipment_destinations_id");
+
+=head1 RELATIONS
+
+=head2 Country
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::Country>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "Country",
+  "Interchange6::Schema::Result::Country",
+  { country_iso_code => "country_iso_code" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 State
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::State>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "State",
+  "Interchange6::Schema::Result::State",
+  { state_id => "state_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 ShipmentMethod
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::ShipmentMethod>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "ShipmentMethod",
+  "Interchange6::Schema::Result::ShipmentMethod",
+  { shipment_methods_id => "shipment_methods_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+1;
---
+++ lib/Interchange6/Schema/Result/ShipmentMethod.pm
@@ -0,0 +1,104 @@
+use utf8;
+package Interchange6::Schema::Result::ShipmentMethod;
+
+=head1 NAME
+
+Interchange6::Schema::Result::ShipmentMethod;
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<shipment_methods>
+
+=cut
+
+__PACKAGE__->table("shipment_methods");
+
+=head1 ACCESSORS
+
+=head2 shipment_methods_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 code
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 255
+
+=head2 type
+
+  data_type: 'numeric'
+  default_value: 0.0
+  is_nullable: 0
+
+=head2 shipment_carriers_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "shipment_methods_id",
+  {
+    data_type => "integer",
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "code",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "name",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 255 },
+  "max_weight",
+  { data_type => "numeric", default_value => "0.0", is_nullable => 0 },
+  "shipment_carriers_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</shipment_methods_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("shipment_methods_id");
+
+=head1 RELATIONS
+
+=head2 ShipmentCarrier
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::ShipmentCarrier>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "ShipmentCarrier",
+  "Interchange6::Schema::Result::ShipmentCarrier",
+  { shipment_carriers_id => "shipment_carriers_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+1;
