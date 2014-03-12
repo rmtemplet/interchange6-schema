114f9d69c31b6971b3ef05742a7e246f0d9be9c0
Sam Batschelet <sbatschelet@mac.com>
2014-03-12 16:41:23 -0400

    Add active field

--- lib/Interchange6/Schema/Result/ShipmentMethod.pm
+++ lib/Interchange6/Schema/Result/ShipmentMethod.pm
@@ -53,6 +53,12 @@ __PACKAGE__->table("shipment_methods");
   is_foreign_key: 1
   is_nullable: 0
 
+head2 active
+
+  data_type: 'boolean'
+  default_value: true
+  is_nullable: 0
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -70,6 +76,8 @@ __PACKAGE__->add_columns(
   { data_type => "numeric", default_value => "0.0", is_nullable => 0 },
   "shipment_carriers_id",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "active",
+  { data_type => "boolean", default_value => \"true", is_nullable => 0 },
 );
 
 =head1 PRIMARY KEY
